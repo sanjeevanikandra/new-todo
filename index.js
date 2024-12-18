b46c9bc78e016b8c117bea5b298a2d8f2037e385
Sanjeevani Kandra <sanjeevanikandra@gmail.com>
2024-12-18 18:44:41 +0530

    secondt commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+const todoRouter = require('./router/todoRouter');
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.Uri)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log(err));
+
+
+
+app.listen(process.env.Port, () => {
+    console.log('Server is running on port 5000');
+});
+
+app.use(todoRouter);
